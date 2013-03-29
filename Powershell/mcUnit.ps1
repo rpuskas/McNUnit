448ffa4604ef902c41465aeee66f5ba82703f20a
rpuskas <rpuskaspublic@gmail.com>
2013-03-28 22:07:23 -0500

    Adds reflection on fixtures for calling individual assemblies

--- Powershell/mcUnit.ps1
+++ Powershell/mcUnit.ps1
@@ -1,18 +1,42 @@
 $ProjectDir = Resolve-Path "..\"
 
 $TestDir = "$ProjectDir\McUnit.TestScenarios\bin\Debug"
-$tests = (Get-ChildItem $TestDir -Recurse -Include *McUnit.TestScenarios.dll)
+$testAssembly = [System.Reflection.Assembly]::LoadFrom((Get-ChildItem $TestDir -Recurse -Include *McUnit.TestScenarios.dll));
 
 $nunitPath = "$ProjectDir\packages\NUnit.Runners.2.6.2\tools"
-$nunitArgs = "$tests"
+$nunitArgs = $testAssembly.Location
 
+function Get-NunitExpression ($path,$fixtures) {
 
-function Get-NunitExpression ($path,$arguments) {
-
-    return "$path\nunit-console.exe $arguments";
+    return "$path\nunit-console.exe $fixtures";
 }
 
+function Get-TestFixtures {
+    param([System.Reflection.Assembly]$assembly)
 
-$job = Start-Job -ScriptBlock { param($expression) Invoke-Expression "$expression"  } -ArgumentList (Get-NunitExpression $nunitPath $nunitArgs)
+    $fixtures = @()
+
+    foreach ($type in $assembly.GetTypes())
+    {
+        $customAttributes = [System.Attribute]::GetCustomAttributes($type)
+
+        foreach ($attribute in $customAttributes) 
+        {
+            if($attribute.TypeId.ToString() -eq "NUnit.Framework.TestFixtureAttribute")
+            {
+                $fixtures += "/fixture:$type "+$assembly.Location;
+            }
+        }
+    }
+
+    return $fixtures;      
+}
+
+$fxs = Get-TestFixtures $testAssembly;
+$exp = Get-NunitExpression $nunitPath $fxs
+write-host $exp;
+
+
+$job = Start-Job -ScriptBlock { param($expression) Invoke-Expression "$expression"  } -ArgumentList $exp
 Wait-Job $job
 Receive-Job -job $job
